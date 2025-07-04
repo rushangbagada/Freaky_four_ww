c4471a8392142a127bf2236cd3b57a06b2bd98b9
rushangbagada <rushang697@gmail.com>
2025-07-04 11:25:41 +0530

    auth

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => res.send("API is running"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
